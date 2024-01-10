7caf593a6a7dfc4d5d5977cd6fd1bbd2dced147f
Adam Carter <adam.clayton.carter@gmail.com>
2024-01-10 13:47:24 -0500

    Initial commit

---
+++ src/main/java/com/example/sq_ch9/SqCh9Application.java
@@ -0,0 +1,13 @@
+package com.example.sq_ch9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqCh9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqCh9Application.class, args);
+	}
+
+}
